1b6e5fbd6bb86abf8738cbd7aad17dffc28aed74
Yuriy Taraday <yorik.sar@gmail.com>
2018-01-30 21:23:33 +0400

    Properly wait until kubectl is operational

--- master.sh
+++ master.sh
@@ -4,7 +4,8 @@ kubeadm init --apiserver-cert-extra-sans $floatingip --token $token
 mkdir -p ~ubuntu/.kube
 cp /etc/kubernetes/admin.conf ~ubuntu/.kube/config
 chown -R ubuntu:ubuntu ~ubuntu/.kube
-until curl -sLo /dev/null http://localhost:8080/swagger-2.0.0.pb-v1; do
+export KUBECONFIG=/etc/kubernetes/admin.conf
+until kubectl get nodes; do
   sleep 2
 done
 kubectl apply -f https://docs.projectcalico.org/v2.6/getting-started/kubernetes/installation/hosted/kubeadm/1.6/calico.yaml
